c728744e1c008c7e2eb80f08e0abe5dbc9890b89
Jonathan Pacheco <jonathan261090@gmail.com>
2015-07-30 20:54:18 -0500

    Initial Commit

---
+++ RestaurantMenuTests/RestaurantMenuTests.m
@@ -0,0 +1,40 @@
+//
+//  RestaurantMenuTests.m
+//  RestaurantMenuTests
+//
+//  Created by Jonathan Pacheco on 30/07/15.
+//  Copyright (c) 2015 Grability. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface RestaurantMenuTests : XCTestCase
+
+@end
+
+@implementation RestaurantMenuTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
